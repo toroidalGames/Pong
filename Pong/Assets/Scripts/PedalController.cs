deb2d0beaa5a36b5687704eec0401af96f511885
Adam-H <adamhigsonpt@outlook.com>
2016-12-03 18:15:36 +0000

    Added functions to move pedal in Y axis up and down, set the bounds for the top and bottom. Added keycode variables enabling the 2 pedal objects to have seperate inputs for their movement.

--- Pong/Assets/Scripts/PedalController.cs
+++ Pong/Assets/Scripts/PedalController.cs
@@ -1,15 +1,34 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class PedalController : MonoBehaviour {
+public class PedalController : MonoBehaviour
+{
+    public float pedalSpeed = 0.1f;
+    private float topBounds;
+    private float bottomBounds;
+    public KeyCode upKey;
+    public KeyCode downKey;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start ()
+	{
+        SetBounds();
+    }
+
+    void SetBounds()
+    {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
+        topBounds = bounds.y - 1.5f;
+        bottomBounds = -bounds.y + 1.5f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(upKey) && transform.position.y < topBounds)
+        {
+            transform.Translate(0f, pedalSpeed*Time.deltaTime, 0);
+        }
+        else if (Input.GetKey(downKey) && transform.position.y > bottomBounds)
+        {
+            transform.Translate(0f, -pedalSpeed*Time.deltaTime, 0);
+        }
+    }
 }
