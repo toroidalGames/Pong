39c2204aa3302479b0353d4c7e9fc9df6137397b
Adam-H <adamhigsonpt@outlook.com>
2016-12-04 13:43:04 +0000

    Added features to ball controller so it shoots off to a certain location on start and able to control its speed

---
+++ Pong/Assets/Scripts/BallController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour
+{
+
+    private float velocity;
+    private Vector3 direction;
+	// Use this for initialization
+	void Start ()
+	{
+	    velocity = 2f;
+	    direction = new Vector3(1,1,0);
+    }   
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = transform.position+direction*velocity*Time.deltaTime ;
+	}
+}
