8010683b3b87fa1abfdd0e84dfca8950356eb695
Adam-H <adamhigsonpt@outlook.com>
2016-12-04 15:01:29 +0000

    Added collider and made it so the ball will reflect in the right direction depending at what vector it hits the paddles or walls at.

--- Pong/Assets/Scripts/BallController.cs
+++ Pong/Assets/Scripts/BallController.cs
@@ -6,16 +6,66 @@ public class BallController : MonoBehaviour
 
     private float velocity;
     private Vector3 direction;
-	// Use this for initialization
-	void Start ()
-	{
-	    velocity = 2f;
-	    direction = new Vector3(1,1,0);
-    }   
-	
-	// Update is called once per frame
-	void Update ()
-	{
-	    transform.position = transform.position+direction*velocity*Time.deltaTime ;
-	}
-}
+    // Use this for initialization
+    void Start()
+    {
+        velocity = 3f;
+        direction = new Vector3(1, 1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + direction*velocity*Time.deltaTime;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collided)
+    {
+        var pedal = collided.gameObject.GetComponent<PedalController>();
+        var wall = collided.gameObject.CompareTag("Wall");
+        if (pedal && direction.x == 1f && direction.y == 1f)
+        {
+            direction = new Vector3(-1, 1, 0);
+            Debug.Log("Collided with " + pedal);
+        }
+        else if (pedal && direction.x == -1f && direction.y == -1f)
+        {
+            direction = new Vector3(1, -1, 0);
+            Debug.Log("Collided with " + pedal);
+        }
+        else if (pedal && direction.x == -1f && direction.y == 1f)
+        {
+            direction = new Vector3(1, 1, 0);
+            Debug.Log("Collided with " + wall);
+        }
+        else if (pedal && direction.x == 1f && direction.y == -1f)
+        {
+            direction = new Vector3(-1, -1, 0);
+            Debug.Log("Collided with " + wall);
+        }
+
+
+        if (wall && direction.x == -1f && direction.y == 1f)
+            {
+                direction = new Vector3(-1, -1, 0);
+                Debug.Log("Collided with " + wall);
+            }
+            else if (wall && direction.x == 1f && direction.y == -1f)
+            {
+                direction = new Vector3(1, 1, 0);
+                Debug.Log("Collided with " + wall);
+            }
+            else if (wall && direction.x == 1f && direction.y == 1f)
+            {
+                direction = new Vector3(1, -1, 0);
+                Debug.Log("Collided with " + wall);
+            }
+            else if (wall && direction.x == -1f && direction.y == -1f)
+            {
+                direction = new Vector3(-1, 1, 0);
+                Debug.Log("Collided with " + wall);
+            }
+        }
+    }
+
